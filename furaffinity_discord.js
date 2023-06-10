d521e10f85a62144096bbd1612efa2cffff4d43a
spacehare <dantath425@gmail.com>
2023-06-10 04:26:33 -0400

    add spoilered link hotkey

--- furaffinity_discord.js
+++ furaffinity_discord.js
@@ -3,32 +3,36 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  grabs FurAffinity submission information to display on Discord
-// @author       https://github.com/spacehare
+// @author       You
 // @match        https://www.furaffinity.net/view/*
 // @grant        none
 // @run-at document-end
 // ==/UserScript==
 
-(function () {
-	"use strict";
-	console.log("🐒 🐇 rabbit's FA tampermonkey script active! 🐇 🐒");
+;(function () {
+	'use strict'
+	console.log("🐒 🐇 rabbit's FA tampermonkey script active! 🐇 🐒")
 	// var discordButton = document.createElement("button");
 	var imageURL =
-		"https:" + document.getElementById("submissionImg").getAttribute("src");
-	var url = document.head.querySelector('meta[property="og:url"]').content;
+		'https:' + document.getElementById('submissionImg').getAttribute('src')
+	var url = document.head.querySelector('meta[property="og:url"]').content
 	var info = document.getElementsByClassName(
-		"classic-submission-title information"
-	)[0];
-	var title = info.children[0].innerHTML;
-	var artist = info.children[1].innerHTML;
+		'classic-submission-title information'
+	)[0]
+	var title = info.children[0].innerHTML
+	var artist = info.children[1].innerHTML
 	var date =
-		Date.parse(document.getElementsByClassName("popup_date")[0].title) / 1000;
+		Date.parse(document.getElementsByClassName('popup_date')[0].title) / 1000
 	document.onkeydown = (e) => {
-		if (e.altKey == true && e.ctrlKey == true && e.key == "f") {
+		if (e.altKey == true && e.ctrlKey == true && e.key == 'f') {
 			navigator.clipboard.writeText(
 				`${imageURL}\n> <${url}>\n> *${title}*\n> by **${artist}**\n> <t:${date}:F>`
-			);
-			console.log("🐇 copied!");
+			)
+		} else if (e.altKey == true && e.ctrlKey == true && e.key == 'h') {
+			navigator.clipboard.writeText(
+				`|| ${imageURL} ||\n>>> <${url}>\n *${title}*\n by **${artist}**\n <t:${date}:F>`
+			)
+			console.log('🐇 copied!')
 		}
-	};
-})();
+	}
+})()
